413aacace19f7b5984c963a82266a84616a8bd8b
modus <illuminatiday@gmail.com>
2017-08-11 09:34:09 +0300

    Delete migrations files

--- db/migrate/20170808103915_add_new_colums_to_events.rb
+++
@@ -1,7 +0,0 @@
-class AddNewColumsToEvents < ActiveRecord::Migration[5.1]
-  def change
-    add_column :events, :description, :text
-    add_column :events, :repeat, :string
-    add_column :events, :user_id, :integer
-  end
-end
--- db/migrate/20170810145642_change_table_user.rb
+++
@@ -1,7 +0,0 @@
-class ChangeTableUser < ActiveRecord::Migration[5.1]
-  def change
-    change_table :users do |t|
-      t.rename :full_name, :username
-    end
-  end
-end
